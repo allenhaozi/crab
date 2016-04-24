e8d8f2fd112d2596d4a82874b3ac258a1d404110
allen.mh <allen.mh@alibaba-inc.com>
2016-04-24 22:29:25 +0800

    add new abstract factory

--- action/Default/Controller/Factory.php
+++ action/Default/Controller/Factory.php
@@ -6,18 +6,27 @@
  *
  * @author allenhaozi@gmail.com
  */
-class Default_Factory extends Controller
+class Default_Controller_Factory extends Controller
 {
-	public function indexAction()
-	{
-		$objCaocao = new Factory_Caocao();
-		$objSword = $objCaocao->createSword();
-		dump( 'caocao use ' . $objSword->getName() . ' kill dongzhuo' );
-		
-		$objCaocao2 = new Factory_Caocao2();
-		$objSword2 = $objCaocao2->createSword();
-		dump( 'caocao use ' . $objSword2->getName() . ' kill dongzhuo' );
-		
-		exit;
-	}
+    public function indexAction()
+    {
+        $objCaocao = new Factory_Caocao();
+        $objSword = $objCaocao->createSword();
+        dump( 'caocao use ' . $objSword->getName() . ' kill dongzhuo' );
+
+        $objCaocao2 = new Factory_Caocao2();
+        $objSword2 = $objCaocao2->createSword();
+        dump( 'caocao use ' . $objSword2->getName() . ' kill dongzhuo' );
+
+        exit;
+    }
+
+    public function kfcAction()
+    {
+        $objFactory = new AF_ChinaKfcFactory();
+        $objCustomer = new AF_Customer( $objFactory );
+        $objCustomer->orderHamburg( 10 );
+        $objCustomer->orderBeverage( 5 );
+        exit;
+    }
 }
