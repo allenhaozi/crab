82691a6e79f0a8e68e2ec7cff8214a63a4b1d83a
allen.mh <allen.mh@alibaba-inc.com>
2016-11-21 15:59:11 +0800

    add log test

--- action/Default/Index.php
+++ action/Default/Index.php
@@ -7,6 +7,7 @@ class Default_IndexController extends Controller
 {
     public function indexAction()
     {
+        Log_Build::test( 'test' );
         $this->assign( 'test',$_GET['test'] );
     }
 }
